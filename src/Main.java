976bbd9ccce5f4bbcb59435dc46aaf07de8b8400
Joakim Wassberg <joakim@arthead.se>
2021-12-07 12:35:51 +0100

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -10,14 +10,14 @@ public class Main {
         try {
             ServerSocket serverSocket = new ServerSocket(9876);
             System.out.println("Waiting for connection...");
-            // Wait for connection
+            // Wait for connection - BLOCKING
             Socket clientSocket = serverSocket.accept();
             // Go a connection
             System.out.println("Got connection from " + clientSocket.getInetAddress());
             BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             PrintWriter output = new PrintWriter(clientSocket.getOutputStream(), true);
             while(true) {
-                // Wait for message from client
+                // Wait for message from client - BLOCKING
                 String message = input.readLine();
                 System.out.println("Got message: " + message);
                 // Exit if client sends the text exit
