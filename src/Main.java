f31975aa539938b6c166a2049f7d5b4b5a3af534
Joakim Wassberg <joakim@arthead.se>
2021-12-07 10:56:41 +0100

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(9876);
+            System.out.println("Waiting for connection...");
+            // Wait for connection
+            Socket clientSocket = serverSocket.accept();
+            // Go a connection
+            System.out.println("Got connection from " + clientSocket.getInetAddress());
+            BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            PrintWriter output = new PrintWriter(clientSocket.getOutputStream(), true);
+            while(true) {
+                // Wait for message from client
+                String message = input.readLine();
+                System.out.println("Got message: " + message);
+                // Exit if client sends the text exit
+                if(message.equals("exit")) {
+                    break;
+                }
+                // Send message to client
+                output.println("You said: " + message);
+            }
+        } catch (IOException e) {
+            System.out.println("Lost connection with client");
+        }
+    }
+}
